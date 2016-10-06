b27657718e061f4c24f9e2684ec9fe08dfb1e2de
sudhatumuluru <sreesudha.tumuluru@gmail.com>
2016-10-06 11:17:47 -0700

    datadog and signalfx working

--- Datadog.py
+++ Datadog.py
@@ -1,17 +1,14 @@
 import bs4, requests, time, subprocess
+from datadog import statsd, initialize, api
 
 while(True):
     res=requests.get('https://www.utctime.net/')
     res.raise_for_status()
     noStarchSoup=bs4.BeautifulSoup(res.text,"html.parser")
-    #print(res.text)
     type(noStarchSoup)
-    print(noStarchSoup.select('span'))
-    print(noStarchSoup.select('span')[2])
     html_elements = noStarchSoup.select('span')[2]
     str(html_elements)
     curr_time = html_elements.getText()
-    print(html_elements.getText())
-    #output=subprocess.check_output(['./signalfx_trial.sh',curr_time[6:]])
+    statsd.gauge('seconds.change',curr_time[6:])
     print(curr_time[6:])
-    time.sleep(3)
+    time.sleep(11)
--- graphite_trial.sh
+++ graphite_trial.sh
@@ -3,3 +3,5 @@
 PORT=2003
 SERVER=graphite-21.jiwiredev.com
 echo "local.random.diceroll 4 `date +%s`" | nc -- ${SERVER} ${PORT}
+
+# echo "local.random.diceroll 4 `date +%s`" | nc -- graphite-21.jiwiredev.com 5555
